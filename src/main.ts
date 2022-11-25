bbdf217152a206e8cfce1cddbb84d82735f30788
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-25 15:14:32 +0100

    fixed await problem

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import './style.scss';
 
 // get books
-const response = await fetch(
-	'https://edwardtanguay.vercel.app/share/techBooks.json'
-);
-const books = await response.json();
+(async () => {
+	const response = await fetch(
+		'https://edwardtanguay.vercel.app/share/techBooks.json'
+	);
+	const books = await response.json();
 
-document.querySelector('#app')!.innerHTML = `
+	document.querySelector('#app')!.innerHTML = `
 	<div>
 		<h2>Tech Books</h2>
 		<div class="books">
@@ -26,3 +27,4 @@ document.querySelector('#app')!.innerHTML = `
 		</div>
 	</div>
 `;
+})();
